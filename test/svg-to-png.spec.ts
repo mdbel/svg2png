d6fd239182e0b98e42eca371c8131488e3202f33
maksdanik <maksdanikovich@gmail.com>
2020-04-25 23:43:12 +0300

    autoformat

--- test/svg-to-png.spec.ts
+++ test/svg-to-png.spec.ts
@@ -3,18 +3,18 @@ import {Svg2Png} from '../src';
 describe('when converting SVG to PNG', () => {
     it('should catch an error when SVG selector is not correct', async () => {
         try {
-         await Svg2Png.toDataURL('blabla');
+            await Svg2Png.toDataURL('blabla');
         } catch (e) {
-            expect(e.message).toBe('SVG not found')
+            expect(e.message).toBe('SVG not found');
         }
     });
 
     it('should catch an error when providing a non-SVG element', async () => {
         const element = document.createElement('div');
         try {
-         await Svg2Png.toDataURL(element as unknown as SVGSVGElement);
+            await Svg2Png.toDataURL(element as unknown as SVGSVGElement);
         } catch (e) {
-            expect(e.message).toBe('Only SVG can be converted to PNG')
+            expect(e.message).toBe('Only SVG can be converted to PNG');
         }
     });
 });
